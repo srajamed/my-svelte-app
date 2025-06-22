85ab78b987155caf030ebf160ba21ad11f529b98
Shanmugesh Raja <shanmugeshraja@mac.lan>
2025-06-22 15:53:41 -0700

    Update blog layout

--- src/Components/Navbar.svelte
+++ src/Components/Navbar.svelte
@@ -32,7 +32,7 @@
   }
 
   nav.scrolled {
-    background-color: rgba(18, 18, 18, 0.50);
+    background-color: rgba(18, 18, 18, 0.40);
     box-shadow: 0 2px 12px rgba(0, 0, 0, 0.35);
     backdrop-filter: blur(6px);
   }
